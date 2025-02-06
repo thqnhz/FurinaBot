812c19cdf5390235b0749c65187b14161584d10d
Nguyen Duy Thanh <89237062+Th4nhZ@users.noreply.github.com>
2025-02-06 17:34:50 +0700

    update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import discord, os, platform, wavelink
 from aiohttp import ClientSession
 from asqlite import Pool
@@ -8,6 +10,23 @@ from typing import List
 from settings import DEFAULT_PREFIX, ACTIVITY_NAME, DEBUG_WEBHOOK
 
 class Furina(Bot):
+    """
+    Customized `commands.Bot` class
+
+    Attributes
+    -----------
+    - pool: `asqlite.Pool`
+        - The database pool for the bot for easier database access
+    - client_session: `aiohttp.ClientSession`
+        - The client session for the bot for easier http request
+
+    Example
+    -----------
+    .. code-block:: python
+        async with aiohttp.ClientSession() as client_session, asqlite.create_pool("config.db") as pool:
+            async with Furina(pool=pool, client_session=client_session) as bot:
+                await bot.start(TOKEN)
+    """
     def __init__(self, *, pool: Pool, client_session: ClientSession) -> None:
         super().__init__(
             command_prefix     = self.get_pre,
@@ -15,34 +34,30 @@ class Furina(Bot):
             strip_after_prefix = True,
             intents            = Intents.all(),
             help_command       = None,
+            allowed_contexts   = app_commands.AppCommandContext(dm_channel=False, guild=True),
             activity           = Activity(type=ActivityType.playing,
                                           name=ACTIVITY_NAME,
                                           state="Playing: N̸o̸t̸h̸i̸n̸g̸")
         )
-
-        # only allow app_commands to be used in servers, so you can't use the bot's app_commands in its dm
-        # change the aguments or comment this out if you wish to use app_commands in bot's dm
-        self.tree.allowed_contexts = app_commands.AppCommandContext(dm_channel=False, guild=True)
-
-        # database pool
         self.pool = pool
-
-        # client session for requests
         self.cs = client_session
 
     async def create_prefix_table(self) -> None:
+        """Create a `custom_prefixes` table in the database"""
         async with self.pool.acquire() as db:
             await db.execute(
                 """CREATE TABLE IF NOT EXISTS custom_prefixes
                    ( guild_id INT NOT NULL PRIMARY KEY, prefix TEXT NOT NULL )""")
             
     async def update_prefixes(self) -> None:
+        """Retrieve all prefixes in the `custom_prefixes` table and cache them in `Furina.prefixes`"""
         async with self.pool.acquire() as db:
             async with db.execute("""SELECT * FROM custom_prefixes""") as cursor:
                 prefixes = await cursor.fetchall()
                 self.prefixes = {prefix[0]: prefix[1] for prefix in prefixes}
             
     def get_pre(self, _, message: discord.Message) -> List[str]:
+        """Custom `get_prefix` method"""
         prefix = self.prefixes.get(message.guild.id) or DEFAULT_PREFIX
         return when_mentioned_or(prefix)(self, message)
 
@@ -61,12 +76,13 @@ class Furina(Bot):
             discord.SyncWebhook.from_url(DEBUG_WEBHOOK).send(embed=embed)
         except ValueError:
             print("Cannot get the Webhook url for on_ready events."
-                  "If you don't want to get a webhook message when the bot is ready, please ignore this.")
+                  "If you don't want to get a webhook message when the bot is ready, please ignore this")
 
     async def setup_hook(self) -> None:
         await self.create_prefix_table()
         await self.update_prefixes()
 
+        # loads the extensions
         for filename in os.listdir("./_extensions"):
             if filename.endswith(".py"):
                 extension = filename[:-3]
@@ -74,7 +90,9 @@ class Furina(Bot):
                     await self.load_extension(f"_extensions.{extension}")
                     print(f"Loaded extension: {extension}")
                 except errors.NoEntryPointError:
-                    print(f"Extension {extension} has no setup function so it cannot be loaded.")
+                    print(f"Extension {extension} has no setup function so it cannot be loaded")
                 except Exception as e:
                     print(f"An error occured when trying to load {extension}\n{e}")
+        await self.load_extension("jishaku")
+        print("Loaded Jishaku extension")
 
