6515de050cb1a716f7a0d20092b422c27b02b424
ThanhZ <ndt1710bt@gmail.com>
2025-02-17 11:43:04 +0700

    interaction.edit_original_response fix

--- _extensions/minigames.py
+++ _extensions/minigames.py
@@ -549,7 +549,7 @@ class Wordle(discord.ui.View):
                                             value=f"{guess.upper()} {interaction.user.mention}", 
                                             description=f"by {interaction.user.display_name}")
                 )
-                return await interaction.edit_original_response(f"Added `{guess}` to help guess list", view=self)
+                return await interaction.edit_original_response(content=f"Added `{guess}` to help guess list", view=self)
             else:
                 return await interaction.followup.send("There are already enough help guesses. Try again later", ephemeral=True)
 
