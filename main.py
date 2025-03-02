2a4c9fc00a56dfb524ca831f005c19e90f6754f5
ThanhZ <ndt1710bt@gmail.com>
2025-03-02 09:48:45 +0700

    better file logging

--- main.py
+++ main.py
@@ -23,11 +23,11 @@ class LogFormatter(logging.Formatter):
     def __init__(self):
         super().__init__()
         self.FORMATS = {
-            logging.DEBUG:    self.grey     + "%(asctime)s | %(levelname)8s" + self.reset + " | %(message)s",
-            logging.INFO:     self.blue     + "%(asctime)s | %(levelname)8s" + self.reset + " | %(message)s",
-            logging.WARNING:  self.yellow   + "%(asctime)s | %(levelname)8s" + self.reset + " | %(message)s",
-            logging.ERROR:    self.red      + "%(asctime)s | %(levelname)8s" + self.reset + " | %(message)s",
-            logging.CRITICAL: self.bold_red + "%(asctime)s | %(levelname)8s" + self.reset + " | %(message)s"
+            logging.DEBUG:    self.grey     + "%(asctime)s | %(levelname)8s" + self.reset + " | %(name)20s : %(message)s",
+            logging.WARNING:  self.yellow   + "%(asctime)s | %(levelname)8s" + self.reset + " | %(name)20s : %(message)s",
+            logging.ERROR:    self.red      + "%(asctime)s | %(levelname)8s" + self.reset + " | %(name)20s : %(message)s",
+            logging.INFO:     self.blue     + "%(asctime)s | %(levelname)8s" + self.reset + " | %(name)20s : %(message)s",
+            logging.CRITICAL: self.bold_red + "%(asctime)s | %(levelname)8s" + self.reset + " | %(name)20s : %(message)s"
         }
 
     def format(self, record):
@@ -38,11 +38,13 @@ class LogFormatter(logging.Formatter):
 
 def handle_setup_logging() -> None:
     """Setup logging for the bot"""
-    from discord import utils
-    timestamp = utils.utcnow().strftime("%Y%m%d_%H%M%S")
-    log_filename = f"logs/furina_{timestamp}.log"
-
-    file_handler = logging.FileHandler(log_filename)
+    import logging.handlers
+    file_handler = logging.handlers.RotatingFileHandler(
+        filename="logs/furina.log",
+        encoding="utf-8",
+        maxBytes=32*1024*1024,
+        backupCount=3
+    )
     console_handler = logging.StreamHandler()
     file_handler.setFormatter(LogFormatter())
     console_handler.setFormatter(LogFormatter())
@@ -52,18 +54,8 @@ def handle_setup_logging() -> None:
     root_logger.addHandler(file_handler)
     root_logger.addHandler(console_handler)
 
-    # utils.setup_logging()
-    
-def delete_old_logs() -> None:
-    """Delete old logs"""
-    logs = sorted([file for file in os.listdir("logs") if file.startswith("furina_") and file.endswith(".log")])
-    if len(logs) > 2:
-        for log in logs[:-2]:
-            os.remove(f"logs/{log}")
-
 async def main() -> None:
     os.makedirs("logs", exist_ok=True)
-    delete_old_logs()
     handle_setup_logging()
     async with ClientSession() as client_session, asqlite.create_pool("config.db") as pool:
         async with FurinaBot(pool=pool, client_session=client_session) as bot:
